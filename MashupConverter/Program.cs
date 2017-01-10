56e58fd595b23ea139a518ec1e829bb496fdc041
Sanggyu Nam <pokeplus@gmail.com>
2017-01-10 14:00:37 +0900

    Reformat Program.cs

--- MashupConverter/Program.cs
+++ MashupConverter/Program.cs
@@ -17,40 +17,39 @@ namespace MashupConverter
         public string OutputFile { get; set; }
     }
 
-	class MainClass
-	{
-		public static void Main(string[] args)
-		{
-		    string pathInput = null;
-		    string pathOutput = null;
-		    var result = Parser.Default.ParseArguments<Options>(args)
-		        .WithParsed(options =>
-		        {
-		            pathInput = options.InputFile;
-		            pathOutput = options.OutputFile;
-		        });
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            string pathInput = null;
+            string pathOutput = null;
+            var result = Parser.Default.ParseArguments<Options>(args)
+                .WithParsed(options =>
+                {
+                    pathInput = options.InputFile;
+                    pathOutput = options.OutputFile;
+                });
 
-		    using (var istream =
-		        null == pathInput ? Console.OpenStandardInput() : File.Open(pathInput, FileMode.Open))
-			using (var ppt = PresentationDocument.Open(istream, false))
-			using (var ostream =
-			    null == pathOutput ? Console.OpenStandardOutput() : File.Open(pathOutput, FileMode.Create))
-			using (var sw = new StreamWriter(ostream))
+            using (var istream =
+                null == pathInput ? Console.OpenStandardInput() : File.Open(pathInput, FileMode.Open))
+            using (var ppt = PresentationDocument.Open(istream, false))
+            using (var ostream =
+                null == pathOutput ? Console.OpenStandardOutput() : File.Open(pathOutput, FileMode.Create))
+            using (var sw = new StreamWriter(ostream))
             using (var writer = new JsonTextWriter(sw))
             using (var generator = new NodeRedFlowGenerator(writer))
-			{
-				var prezPart = ppt.PresentationPart;
-				var slideIds = prezPart.Presentation.SlideIdList.ChildElements;
-			    var slideParts = from sid in slideIds
-			        select (SlidePart) prezPart.GetPartById(((SlideId) sid).RelationshipId);
-			    foreach (var sp in slideParts)
-			    {
-			        var activity = new Activity(sp);
-			        generator.Add(activity);
-			    }
-			    generator.Generate();
-			}
-		}
-	}
-
+            {
+                var prezPart = ppt.PresentationPart;
+                var slideIds = prezPart.Presentation.SlideIdList.ChildElements;
+                var slideParts = from sid in slideIds
+                    select (SlidePart) prezPart.GetPartById(((SlideId) sid).RelationshipId);
+                foreach (var sp in slideParts)
+                {
+                    var activity = new Activity(sp);
+                    generator.Add(activity);
+                }
+                generator.Generate();
+            }
+        }
+    }
 }
